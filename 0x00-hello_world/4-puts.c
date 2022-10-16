d4403468ac0661045f117955ad79a6f7e6c77419
NanjeH <helgananje8@gmail.com>
2022-10-16 12:47:24 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,13 @@
-gcc puts 0
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n")
+	       return 0;
+}
+	   
+	      	
