2fd4099925324aaf53e78c1609cbadabe4a27465
NanjeH <helgananje8@gmail.com>
2023-02-19 16:21:35 -0800

    my commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - a program that prints a single digit
+ *
+ * Return: Always 0
+ */
+int main(void)
+
+{
+	int n;
+
+for (n = 0; n < 10; n++)
+
+	{
+putchar('n');
+
+}
+putchar('\n');
+
+return (0);
+}
