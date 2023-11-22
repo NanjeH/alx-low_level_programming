7e33fa5121f8a7077756bb41895a23fba9306fd3
NanjeH <helgananje8@gmail.com>
2023-11-22 04:06:12 -0800

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,9 @@
  * @argv: array of arguements
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
+	() argv;
 	printf("%d\n", argc - 1);
 	return (0);
 }
