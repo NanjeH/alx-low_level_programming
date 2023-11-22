baefaa183bd82e76d5f8ad30401ef13bcf477a48
NanjeH <helgananje8@gmail.com>
2023-11-22 03:56:29 -0800

    update

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - prints number of arguements in program
+ * @argc: number of arguements
+ * @argv: array of arguements
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
